d707e8b4b6ad3b1888ae5c85287518be3e3c9163
Ilya Lebedev <bacherko@gmail.com>
2019-08-20 13:52:41 +0300

    fix

--- src/actions/charge.js
+++ src/actions/charge.js
@@ -2,5 +2,5 @@ import Basic from './basic';
 
 export default class Charge extends Basic {
   _contractAccount = 'gls.charge';
-  _contractActions = ['setrestorer', 'use', 'usenotifygt', 'usenotifylt', 'useandstore', 'removestored'];
+  _contractActions = ['setrestorer'];
 }
